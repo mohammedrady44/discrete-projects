9c7cd871a3ba7d8d86fa4c4070be9789641f298b
mohammedrady44 <121281287+mohammedrady44@users.noreply.github.com>
2022-12-23 17:59:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, i, sum;
+            Console.WriteLine(" Enter the first value : ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the last value : ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Perfect numbers are : ");
+            for (n = x; n <= y; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+            }
+        }
+    }
+}
